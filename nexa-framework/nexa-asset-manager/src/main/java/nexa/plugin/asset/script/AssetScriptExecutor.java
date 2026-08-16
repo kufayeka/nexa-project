09413fcb5c20f980f8d7783fd31ef1d155930762
Yeka Kufa <110327537+kufayeka@users.noreply.github.com>
2026-08-16 21:08:39 +0700

    refactor: isolate asset scripting engine

--- nexa-framework/nexa-asset-manager/src/main/java/nexa/plugin/asset/script/AssetScriptExecutor.java
+++ nexa-framework/nexa-asset-manager/src/main/java/nexa/plugin/asset/script/AssetScriptExecutor.java
@@ -1,64 +1,41 @@
 package nexa.plugin.asset.script;
 
-import nexa.framework.runtime.api.model.RuntimeMessage;
-import nexa.framework.runtime.domain.scripting.api.ScriptExecutionControl;
-import nexa.framework.runtime.domain.scripting.internal.nexa.NexaParser;
-import nexa.framework.runtime.domain.scripting.internal.nexa.NexaProgram;
-import nexa.framework.runtime.domain.scripting.internal.nexa.NexaRuntime;
-import nexa.framework.runtime.domain.scripting.internal.nexa.NexaTokenizer;
 import nexa.plugin.asset.resource.AssetManagerResourcePlugin;
 
-import java.lang.reflect.Method;
-
+/**
+ * Compatibility facade for existing Asset Manager call sites.
+ * The actual compiler/runtime/cache now lives in {@link AssetScriptingEngine}.
+ */
+@Deprecated(forRemoval = true)
 public final class AssetScriptExecutor {
+    private AssetScriptExecutor() {
+    }
 
-    public static Object executeCalculation(String script, String attributePath, Object currentValue, Object oldValue, Object newValue) {
+    public static Object executeCalculation(
+        String script,
+        String attributePath,
+        Object currentValue,
+        Object oldValue,
+        Object newValue
+    ) {
+        AssetManagerResourcePlugin manager = AssetManagerResourcePlugin.getActiveInstance();
+        if (manager == null) {
+            throw new IllegalStateException("Asset Manager plugin belum aktif atau tidak dapat ditemukan.");
+        }
+
+        AssetScriptingEngine engine = AssetScriptingEngine.forManager(manager);
+        AssetManagerResourcePlugin.setInsideCalculationScript(true);
         try {
-            ScriptContextTracker.setContextPath(attributePath);
-            AssetManagerResourcePlugin.setInsideCalculationScript(true);
-            ScriptContextTracker.startTrackingReads();
-            
-            ScriptSelfContext.setContext(new ScriptSelfContext.Self(
-                currentValue, 
-                oldValue, 
-                newValue, 
-                System.currentTimeMillis(), 
-                "GOOD"
-            ));
-
-            NexaTokenizer tokenizer = new NexaTokenizer(script);
-            NexaParser parser = new NexaParser(tokenizer.tokenize());
-            NexaProgram program = parser.parseProgram();
-
-            ScriptExecutionControl control = new ScriptExecutionControl((port, msg) -> {});
-            NexaRuntime runtime = new NexaRuntime(new RuntimeMessage(), control);
-
-            Object resultVal = null;
-            try {
-                runtime.executeStatements(program.statements());
-            } catch (RuntimeException e) {
-                if (e.getClass().getSimpleName().equals("ReturnSignal")) {
-                    Method valueMethod = e.getClass().getDeclaredMethod("value");
-                    valueMethod.setAccessible(true);
-                    resultVal = valueMethod.invoke(e);
-                } else {
-                    throw e;
-                }
-            }
-
-            AssetManagerResourcePlugin manager = AssetManagerResourcePlugin.getActiveInstance();
-            if (manager != null) {
-                manager.registerDependencies(attributePath, ScriptContextTracker.getTrackedReads());
-            }
-
-            return resultVal;
-        } catch (Exception e) {
-            throw new RuntimeException("Error executing asset calculation script for " + attributePath + ": " + e.getMessage(), e);
+            return engine.executeCalculation(
+                manager,
+                script,
+                attributePath,
+                currentValue,
+                oldValue,
+                newValue
+            );
         } finally {
-            ScriptContextTracker.stopTrackingReads();
-            ScriptContextTracker.clearContext();
             AssetManagerResourcePlugin.setInsideCalculationScript(false);
-            ScriptSelfContext.clearContext();
         }
     }
 }
