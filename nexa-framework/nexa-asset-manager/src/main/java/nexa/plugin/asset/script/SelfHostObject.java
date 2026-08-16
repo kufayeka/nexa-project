e88458d432cbb02d6d82d44a187d42bcd5cd4432
Yeka Kufa <110327537+kufayeka@users.noreply.github.com>
2026-08-16 21:07:42 +0700

    refactor: isolate asset scripting engine

--- nexa-framework/nexa-asset-manager/src/main/java/nexa/plugin/asset/script/SelfHostObject.java
+++ nexa-framework/nexa-asset-manager/src/main/java/nexa/plugin/asset/script/SelfHostObject.java
@@ -2,23 +2,36 @@ package nexa.plugin.asset.script;
 
 import nexa.framework.runtime.domain.scripting.internal.nexa.NexaHostObject;
 import nexa.framework.runtime.domain.scripting.internal.nexa.NexaScriptException;
+import nexa.plugin.asset.resource.AssetManagerResourcePlugin;
 
 public final class SelfHostObject implements NexaHostObject {
 
     @Override
     public Object member(String name, int line, int column) {
-        ScriptSelfContext.Self self = ScriptSelfContext.getContext();
-        if (self == null) {
-            throw new NexaScriptException("Object 'self' hanya bisa diakses di dalam runtime script attribute calculation.", line, column);
+        AssetManagerResourcePlugin manager = AssetManagerResourcePlugin.getActiveInstance();
+        AssetScriptingEngine engine = manager != null ? manager.getScriptingEngine() : null;
+        AssetScriptContext context = engine != null ? engine.currentContext() : null;
+
+        if (context == null) {
+            throw new NexaScriptException(
+                "Object 'self' hanya bisa diakses di dalam runtime script attribute calculation.",
+                line,
+                column
+            );
         }
 
+        AssetScriptContext.Self self = context.self();
         return switch (name) {
             case "value" -> self.value();
             case "oldValue" -> self.oldValue();
             case "newValue" -> self.newValue();
             case "timestamp" -> self.timestamp();
             case "quality" -> self.quality();
-            default -> throw new NexaScriptException("Property 'self." + name + "' tidak didukung.", line, column);
+            default -> throw new NexaScriptException(
+                "Property 'self." + name + "' tidak didukung.",
+                line,
+                column
+            );
         };
     }
 }
