3f030345f014c24a877deac4e9a38b0b242cb270
Yeka Kufa <110327537+kufayeka@users.noreply.github.com>
2026-08-17 22:20:10 +0700

    test: fix missing Map imports in bytecode compiler tests

--- nexa-framework/nexa-compiler/src/test/java/nexa/compiler/codegen/NexaBytecodeCompilerTest.java
+++ nexa-framework/nexa-compiler/src/test/java/nexa/compiler/codegen/NexaBytecodeCompilerTest.java
@@ -9,7 +9,9 @@ import nexa.framework.runtime.api.model.RuntimeMessage;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
